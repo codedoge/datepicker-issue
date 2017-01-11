789750cd5f7273f37d968a3d9ae11b5db874de19
Luigi Fortunati <luigi.fortunati@gmail.com>
2017-01-11 14:40:32 +0100

    Added datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {DpDatePickerModule} from "ng2-date-picker";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DpDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
